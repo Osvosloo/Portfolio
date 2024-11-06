807432862605241fbb378d7cbe8dfb1a041e1b49
osvosloo <osvosloo2@gmail.com>
2024-11-06 08:50:54 +0200

    UI Updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,13 @@
-import React from "react";
+// import React from "react";
 import ".././styles/Hero.css";
-import heroImage from ".././assets/monkeHeadshot.jfif";
+// import heroImage from ".././assets/monkeHeadshot.jfif";
 import { Link } from "react-scroll";
 
 function Hero() {
   return (
     <section className="hero">
       <div className="hero-text">
-        <h1>I'm Stephan, but you can call me Vossie!</h1>
+        <h1>I&apos;m Stephan, but you can call me Vossie!</h1>
         <p></p>
         <button className="hero-button">
           <Link to="contact-me" smooth={true} duration={400}>
@@ -15,9 +15,9 @@ function Hero() {
           </Link>{" "}
         </button>
       </div>
-      <div className="hero-image">
+      {/* <div className="hero-image">
         <img src={heroImage} alt="Hero" />
-      </div>
+      </div> */}
     </section>
   );
 }
